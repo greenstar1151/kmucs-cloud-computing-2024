6ab8e56ab530f67b7718bcc4607f26c7d926cf8c
Greenstar <39073901+greenstar1151@users.noreply.github.com>
2024-10-29 15:18:12 +0900

    chore(iac): target group 등록 딜레이를 줄이기 위해 헬스체크 정책 수정

--- infra/alb.tf
+++ infra/alb.tf
@@ -7,11 +7,22 @@ resource "aws_lb" "web_alb" {
 }
 
 resource "aws_lb_target_group" "web_tg" {
-  name        = "${var.resource_name_prefix}-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = module.vpc.vpc_id
-  target_type = "instance"
+  name                 = "${var.resource_name_prefix}-tg"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = module.vpc.vpc_id
+  target_type          = "instance"
+  deregistration_delay = 30
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 10
+  }
 }
 
 resource "aws_lb_listener" "web_listener" {
